90cb8e9f1f23bca57c4158d5ef60878295650482
Moon_hyeonseung <ansgustmd5010@naver.com>
2026-08-24 08:38:02 +0900

    chagnge_to_arm

--- boot/boot.s
+++ boot/boot.s
@@ -1,31 +1,32 @@
-.section .multiboot
-.align 4
+.section ".text.boot"
 
-.long 0x1BADB002
-.long 0
-.long -(0X1BADB002)
-
-.section .bss
-.align 16
-
-stack_bottom:
-.skip 16384
-stack_top:
-
-.section .text
 .global _start
-.type _start, @function
 
 _start:
-	mov $stack_top, %esp
+	mrs x0, mpidr_el1
+	and x0, x0, #0xFF
+	cbz x0, master_core
 
-	push %ebx
-	push %eax
+hang:
+	wfe
+	b hang
 
-	call kernel_main
+master_core:
+	ldr x1, =_bss_start
+	ldr x2, =_bss_end
+
+bss_clear_loop:
+	cmp x1,x2
+	b.ge init_stack
+	str xzr, [x1], #8
+	b bss_clear_loop
+
+init_stack:
+	ldr x0, =_start
+	mov sp, x0
+
+	bl kernel_main
 
 halt:
-	cli
-	hlt
-	jmp halt
-
+	wfi
+	b halt
