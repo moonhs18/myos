80ba1f85f8c20ce85bc7c6cf9861c9613f43bdeb
Moon_hyeonseung <ansgustmd5010@naver.com>
2026-08-24 13:20:42 +0900

    uart_test

--- boot/boot.s
+++ boot/boot.s
@@ -1,5 +1,4 @@
 .section ".text.boot"
-
 .global _start
 
 _start:
@@ -11,22 +10,56 @@ hang:
 	wfe
 	b hang
 
-master_core:
-	ldr x1, =_bss_start
-	ldr x2, =_bss_end
+el_setup:
+	mrs x0, CurrentEL
+	lsr x0, x0, #2
 
-bss_clear_loop:
-	cmp x1,x2
-	b.ge init_stack
-	str xzr, [x1], #8
-	b bss_clear_loop
+	cmp x0, #3
+	b.eq level_el3
+	cmp x0, #2
+	b.eq level_el2
+	cmp x0, #1
+	b init_kernel_sp
+	
+level_el3:
+	mov x1, #(1<<1024)
+	msr scr_el3, x1
 
-init_stack:
-	ldr x0, =_start
+	mov x1, #0b00101
+	msr spsr_el3, x1
+	
+	adr x1, init_kernel_sp
+	msr elr_el3, x1
+	
+	eret
+
+level_el2:
+	mov x1, #(1<<31)
+	msr hcr_el2, x1
+
+	mov x1, #0b00101
+	msr spsr_el2, x1
+	
+	adr x1, init_kernel_sp
+	msr elr_el2, x1
+	
+	eret
+
+init_kernel_sp:
+	ldr x0, =_stack_top
 	mov sp, x0
 
-	bl kernel_main
+	ldr x1, = _bss_start
+	ldr x2, = _bss_end
 
-halt:
+clear_bss:
+	cmp x1, x2
+	b.ge enter_kernel
+	str xzr, [x1], #8
+	b clear_bss
+
+enter_kernel:
+	bl kernel_main
+system_halt:
 	wfi
-	b halt
+	b system_halt
---
+++ uart/uart.h
@@ -0,0 +1,23 @@
+#ifndef UART_H
+#define UART_H
+
+#include <stdint.h>
+
+#define UART0_BASE 0x09000000
+
+#define UARTDR      ((volatile uint32_t *)(UART0_BASE + 0x00)) //Data Register for rx tx
+#define UARTFR      ((volatile uint32_t *)(UART0_BASE + 0x18)) //Flag Register for state flag
+#define UARRIBRD    ((volatile uint32_t *)(UART0_BASE + 0x24)) //Interger Baud Rate Divisior
+#define UARTFBRD    ((volatile uint32_t *)(UART0_BASE + 0x28)) //Fractional Baud Rate Divisior
+#define UARTLCR_H   ((volatile uint32_t *)(UART0_BASE + 0x2C)) //Line Control Register
+#define UARTCR      ((volatile uint32_t *)(UART0_BASE + 0x30)) //Control Register
+
+#define FR_TXFF (1<<5) //Transmit FIFO Full
+#define FR_RXFE (1<<4) //Receive FIFO Empty
+
+void uart_init(void);
+void uart_putc(char c);
+void uart_puts(const char *s);
+void uart_put_hex(uint64_t val);
+
+#endif
